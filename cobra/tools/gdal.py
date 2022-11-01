f04c61db754028b40274e23bbe5259b6922cd729
Marco Bradtke <marco@bradtke.eu>
2022-11-01 11:33:21 +0100

    get gdal working

--- cobra/tools/gdal.py
+++ cobra/tools/gdal.py
@@ -7,10 +7,11 @@ import subprocess
 
 class GdalJob:
     
-    def __init__(self, args):
+    def __init__(self, args, job_type):
         
         self.args = args
         self.id = uuid.uuid1()
+        self.job_type = job_type
 
 
 class GdalEngine:
@@ -25,6 +26,7 @@ class GdalEngine:
         
     def listen(self):
         
+        print('Listen')
         self.l.debug('listen')
         while(True):
             time.sleep(5)
@@ -40,17 +42,33 @@ class GdalEngine:
         self.busy = True
         self.l.debug(job.args)
         try: 
+
+            if job.job_type == 'generic':
+
+                return_value = self.handle_generic_job(job)
+            
             return_value = subprocess.run(job.args)
         
             if return_value.returncode == 0:
-                self.l.info(f'Job {job_id} - export to {filename} finished successfully')
+                self.l.info(f'Job {job.id} finished successfully')
 
             else: 
-                self.l.error(f'Error in {job_id} - export {filename} failed')
+                self.l.error(f'Error in {job.id} failed')
 
-        
         finally:
             self.busy = False
+
+    def handle_generic_job(self, job):
+
+        self.l.info(f'Handle generic job')
+
+        return_value = subprocess.run(job.args)
+        print(return_value)
+        return return_value
+
+    def load_shape(self, job):
+
+        self.l.info(f'load_shape ')
 
 class GdalClient:
     
