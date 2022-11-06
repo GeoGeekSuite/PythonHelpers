f44a955c13bf442a9be2949ba7a35ae1d217ea02
Marco Bradtke <marco@bradtke.eu>
2022-11-06 23:12:55 +0100

    osm2pgsql job working

--- cobra/tools/job.py
+++ cobra/tools/job.py
@@ -1,10 +1,28 @@
 from cobra.helper.logging import Logger
 import uuid
+import pika
+import pickle
 
 class Job:
     
-    def __init__(self, args, job_type=None):
+    def __init__(self, args, job_queue=None, job_type=None):
         
         self.args = args
         self.id = uuid.uuid1()
         self.job_type = job_type
+        self.job_queue = job_queue
+
+class JobClient:
+
+    def __init__(self):
+        
+        self.l = Logger(self)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+        self.channel = self.connection.channel()
+        
+        
+    def _send_message(self, job):
+        
+        self.l.debug('send')
+        self.channel.queue_declare(queue=job.job_queue)
+        self.channel.basic_publish(exchange='', routing_key=job.job_queue, body=pickle.dumps(job))
--- cobra/tools/osm2pgsql.py
+++ cobra/tools/osm2pgsql.py
@@ -2,12 +2,14 @@ from cobra.helper.logging import Logger
 from cobra.tools.job import Job
 from cobra.tools.job_engine import JobEngine
 import time
+import subprocess
 
 class Osm2pgsqlJob(Job):
 
     def __init__(self, path_to_file, style_file = None, args=[]):
 
-        super().__init__(args, job_type='osm2pgsql')
+        super().__init__(args, job_type='osm2pgsql', job_queue='osm2pgsql')
+        self.path_to_file = path_to_file
 
 class Osm2pgsqlEngine(JobEngine):
 
@@ -17,4 +19,11 @@ class Osm2pgsqlEngine(JobEngine):
 
     def int_handle_job(self, job):
 
-        self.l.debut('int_handle_job')
+        #TODO: Check if postgis extenstion is enabled
+
+        self.l.debug('int_handle_job')
+        #  args =['osm2pgsql','-c','-S', f'/styles/{style}', f'--output-pgsql-schema={schema}', '--slim', path_to_pbf]
+        args =['osm2pgsql','-c','-S', f'/download/default.style', '--slim', job.path_to_file]
+        print(args)
+        return_value = subprocess.run(args)
+        print(return_value)
